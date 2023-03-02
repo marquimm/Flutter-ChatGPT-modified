006feb0b368539656febe479b2f2059d21f5d345
marquimm <marco.marquesp@gmail.com>
2023-03-02 10:15:21 -0300

    Update http_setup.dart

--- lib/src/model/client/http_setup.dart
+++ lib/src/model/client/http_setup.dart
@@ -3,7 +3,7 @@ class HttpSetup {
   final int connectTimeout;
   final int receiveTimeout;
 
-  HttpSetup({this.sendTimeout = 5000, this.connectTimeout = 5000, this.receiveTimeout = 5000});
+  HttpSetup({this.sendTimeout = 50000, this.connectTimeout = 50000, this.receiveTimeout = 50000});
 
   HttpSetup getHttpSetup() => HttpSetup();
 }
